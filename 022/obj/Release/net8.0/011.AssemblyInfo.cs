11d0295b3d2da860a050643fcfbd06ecba552a31
cdubbins <chris@1downsolutions.com>
2025-12-04 07:40:07 -0500

    Days 2 and 3

--- 022/obj/Release/net8.0/011.AssemblyInfo.cs
+++ 022/obj/Release/net8.0/011.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("011")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ca34854248a2618b8dbc37b126809a72a6c0d0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("011")]
 [assembly: System.Reflection.AssemblyTitleAttribute("011")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
